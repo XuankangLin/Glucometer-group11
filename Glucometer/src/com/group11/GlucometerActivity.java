433957c26d57713daaf56441855d36b2ec88b2d6
Andriy Lin <lxk19921205@gmail.com>
2011-11-24 13:01:48 +0800

    first init

---
+++ Glucometer/src/com/group11/GlucometerActivity.java
@@ -0,0 +1,13 @@
+package com.group11;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlucometerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
