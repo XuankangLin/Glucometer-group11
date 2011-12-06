7b5276a346c7a28027a383c0001133f5f9c52c49
Andriy_Lin <lxk19921205@gmail.com>
2011-12-06 22:23:33 +0800

    add a new class Screen

---
+++ Glucometer/src/com/group11/Screen.java
@@ -0,0 +1,11 @@
+package com.group11;
+
+import android.app.Activity;
+
+/**
+ * This class is contained in the glucometer image 
+ * and is responsible for displaying data & status
+ */
+public class Screen extends Activity {
+
+}
