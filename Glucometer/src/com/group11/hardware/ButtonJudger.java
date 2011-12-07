959a4a9e2ff9c5f44c752431bcb60c7e46237e59
Andriy_Lin <lxk19921205@gmail.com>
2011-12-07 16:10:50 +0800

    add a button judger class to deal with interrupt judgement

---
+++ Glucometer/src/com/group11/hardware/ButtonJudger.java
@@ -0,0 +1,9 @@
+package com.group11.hardware;
+
+/**
+ * it deals with the interrupts sent when the sole button is clicked
+ * and decides what kind of button click it is
+ */
+public class ButtonJudger {
+
+}
