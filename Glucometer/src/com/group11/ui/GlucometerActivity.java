e1393e26632a353cf4b3fe4790a1359b2756e8e4
Andriy Lin <lxk19921205@gmail.com>
2011-12-09 22:06:37 +0800

    partly finish the layout of screen, waiting for the Result area.

--- Glucometer/src/com/group11/ui/GlucometerActivity.java
+++ Glucometer/src/com/group11/ui/GlucometerActivity.java
@@ -7,6 +7,7 @@ import com.group11.R;
 import com.group11.base.ClickStyle;
 import com.group11.hardware.Beeper;
 import com.group11.util.ClickJudger;
+import com.group11.util.TimeFormatter;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -19,6 +20,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static com.group11.base.Interrupt.*;
@@ -31,7 +34,23 @@ public class GlucometerActivity extends Activity {
 	private ImageView testStripImage;
 	private ImageView resetImage;
 	private ImageView usbImage;
+	private ImageView acPlugImage;
+
+	private ImageView batteryImage;
 	private ImageView acImage;
+	private ImageView testingModeImage;
+	private ImageView browsingModeImage;
+	private ImageView uploadingModeImage;
+	private ImageView errorModeImage;
+	private LinearLayout topPanel;
+
+	private LinearLayout resultPanel;
+
+	private ImageView progressBarImage;
+
+	private TextView dateText;
+	private TextView timeText;
+	private LinearLayout datePanel;
 	
 	
 	private Handler handler = new Handler(new GlucometerHandlerCallback());
@@ -51,7 +70,33 @@ public class GlucometerActivity extends Activity {
         testStripImage = (ImageView) findViewById(R.id.testStripImage);
         resetImage = (ImageView) findViewById(R.id.resetImage);
         usbImage = (ImageView) findViewById(R.id.usbImage);
+        acPlugImage = (ImageView) findViewById(R.id.acPlugImage);
+        
+        batteryImage = (ImageView) findViewById(R.id.batteryImage);
         acImage = (ImageView) findViewById(R.id.acImage);
+        testingModeImage = (ImageView) findViewById(R.id.testingModeImage);
+        browsingModeImage = (ImageView) findViewById(R.id.browsingModeImage);
+        uploadingModeImage = (ImageView) findViewById(R.id.uploadingModeImage);
+        errorModeImage = (ImageView) findViewById(R.id.errorModeImage);
+        topPanel = (LinearLayout) findViewById(R.id.topPanel);
+        
+        resultPanel = (LinearLayout) findViewById(R.id.resultPanel);
+        
+        progressBarImage = (ImageView) findViewById(R.id.progressBarImage);
+        
+        dateText = (TextView) findViewById(R.id.dateText);
+        timeText = (TextView) findViewById(R.id.timeText);
+        datePanel = (LinearLayout) findViewById(R.id.datePanel);
+        
+        //TODO testing code, to be deleted
+        batteryImage.setVisibility(View.VISIBLE);
+        acImage.setVisibility(View.VISIBLE);
+        testingModeImage.setVisibility(View.VISIBLE);
+        browsingModeImage.setVisibility(View.VISIBLE);
+        uploadingModeImage.setVisibility(View.VISIBLE);
+        errorModeImage.setVisibility(View.VISIBLE);
+        dateText.setText(TimeFormatter.formatDate(new Date()));
+        timeText.setText(TimeFormatter.formatTime(new Date()));
         
         this.setOnClickListeners();
         
@@ -116,7 +161,7 @@ public class GlucometerActivity extends Activity {
 			}
 		});
 
-        acImage.setOnClickListener(new OnClickListener() {
+        acPlugImage.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -126,59 +171,85 @@ public class GlucometerActivity extends Activity {
 		});
     }
     
+    /**
+     * set the screen parts to be visible or not
+     * @param visible
+     */
+    private void setScreenVisible(boolean visible) {
+    	if (visible) {
+			topPanel.setVisibility(View.VISIBLE);
+			resultPanel.setVisibility(View.VISIBLE);
+			progressBarImage.setVisibility(View.VISIBLE);
+			datePanel.setVisibility(View.VISIBLE);
+		}
+    	else {
+			topPanel.setVisibility(View.INVISIBLE);
+			resultPanel.setVisibility(View.INVISIBLE);
+			progressBarImage.setVisibility(View.INVISIBLE);
+			datePanel.setVisibility(View.INVISIBLE);
+		}
+    }
+    
     private void doStripInserted() {
-    	//TODO
+    	//TODO fulfill this method
     }
     
     private void doStripPulledOut() {
-    	//TODO
+    	//TODO fulfill this method
     }
     
     private void doUSBConnected() {
-    	//TODO
+    	//TODO fulfill this method
     }
 
 	private void doUSBDisconnected() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doStripValid() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doStripInvalid() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doBloodSufficient() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doBloodInsufficient() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doResultReady() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doAcOn() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doAcOff() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doTimeTick() {
-		//TODO
+		//TODO fulfill this method
 	}
 	
 	private void doButtonClicked(Message msg) {
-		//TODO
+		//TODO testing code, to be deleted
 		ClickStyle style = ClickStyle.get(msg.arg1);
 		Log.i("CLICK_STYLE", style.toString());
 		Toast.makeText(this, style.toString(), 50).show();
+		
+		if (style == ClickStyle.DOUBLE_CLICK) {
+			setScreenVisible(true);
+		}
+		else if (style == ClickStyle.SHORT_CLICK) {
+			setScreenVisible(false);
+		}
 	}
 	
 	/**
